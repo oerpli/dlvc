60b28b25bdddd6c8e3ae013404e86b05d5d6e451
alg0627958 <gl@mercado.co.at>
2016-11-04 13:00:49 +0100

    Added H5 functioality

--- src/knn_classify_hog_tinycifar10.py
+++ src/knn_classify_hog_tinycifar10.py
@@ -1,18 +1,21 @@
 from ImageVectorizer import ImageVectorizer
-from TinyCifar10Dataset import TinyCifar10Dataset
+from HDF5FeatureVectorDataset import HDF5FeatureVectorDataset
 from GridSearch import GridSearch
 
-# The actual script
-ratio = 0.01; #Default is 0.1
+ # must match TinyCifar10Dataset
+cifar10_classnames = {  0: 'airplane',
+                        1: 'automobile', 
+                        2: 'bird', 
+                        3: 'cat', 
+                        4: 'deer', 
+                        5: 'dog', 
+                        6: 'frog', 
+                        7: 'horse', 
+                        8: 'ship', 
+                        9: 'truck'} 
 
-
-trainData = TinyCifar10Dataset("../data",'train',ratio)
-valData = TinyCifar10Dataset("../data",'val',ratio)
-testData = TinyCifar10Dataset("../data",'test',ratio)
-trainSet = ImageVectorizer(trainData)
-valSet = ImageVectorizer(valData)
-testSet = ImageVectorizer(testData)
-
+trainSet = HDF5FeatureVectorDataset('features_tinycifar10_train.h5',cifar10_classnames)
+valSet = HDF5FeatureVectorDataset('features_tinycifar10_val.h5',cifar10_classnames)
+testSet = HDF5FeatureVectorDataset('features_tinycifar10_test.h5',cifar10_classnames)
 
 GridSearch().gridSearch(trainSet, valSet, testSet);
-print() # only here to set breakpoint
