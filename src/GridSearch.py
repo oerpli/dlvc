2f05e1f26277f9b7c61ddb734bb3a58bc7f3a394
alg0627958 <gl@mercado.co.at>
2016-11-04 16:30:19 +0100

    added comments

--- src/GridSearch.py
+++ src/GridSearch.py
@@ -1,18 +1,22 @@
 import FeatureVectorDataset as f
 from KnnClassifier import KnnClassifier
 
+# This Class implements the knn Grid Search for different 
+# parameter for k and the distance norm
 
 class GridSearch:
-    norms = []
+    norms = []      
     K = range(1,5)
     defer_init = False
 
-    def defaultValues(self,trainSet): # default if no values provided
-        minK = 1 # 1-nn
+    def defaultValues(self,trainSet): 
+        # default if no values provided, calculates suitable ranges for k
+        minK = 1 # 1-nn  
         maxK = min(50, max(5, round(trainSet.size() / 50))) # find a suitable maximal k value
         maxSteps = 20  # should not do more step than that
         step = max(round(maxK / maxSteps - 0.5),1) # skip some values if maxK too big
         self.norms = ['l1','l2']
+
         self.K = range(minK, maxK, step)
 
     def __init__(self, k_range=-1 , cmp_range=-1):
@@ -32,8 +36,6 @@ class GridSearch:
             prediction = classifier.predict(sample[0])
             if (prediction == sample[1]):
                 correctPredictions = correctPredictions + 1
-            #print('{2}: Predicted {0}, should be
-            #{1}'.format(prediction,sample[1],i))
         accuracy = correctPredictions / predictSet.size()
         return accuracy
 
@@ -45,7 +47,7 @@ class GridSearch:
         print("  [train] {0} samples".format(trainSet.size()))
         print("  [val] {0} samples".format(valSet.size()))
 
-        bestResult = (-1, 0, 0)
+        bestResult = (-1, 0, 0)  # Array to store the best result so far
         for norm in self.norms:
             for k in self.K:
                 accuracy = self.classify(trainSet,valSet,KnnClassifier(k,norm))
@@ -53,8 +55,11 @@ class GridSearch:
                 if (accuracy > bestResult[0]):
                     bestResult = (accuracy, k, norm)
 
+        # Now test the best parameters on the test set.
+
         bestK = bestResult[1]
         bestNorm = bestResult[2]
+
         print("Testing best combination ({}, {}) on test set ...".format(bestK, bestNorm))
         print("  [test] {0} samples".format(testSet.size()))
         testAccuracy = self.classify(trainSet,testSet,KnnClassifier(bestK, bestNorm))
