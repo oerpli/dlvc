82327f74647cdd6a5cec2cf23fb44a038a41e48e
Abraham Hinteregger <oerpli@hotmail.com>
2016-11-04 15:07:32 +0100

    default split ratio 0.1

--- src/knn_classify_tinycifar10.py
+++ src/knn_classify_tinycifar10.py
@@ -3,16 +3,16 @@ from TinyCifar10Dataset import TinyCifar10Dataset
 from GridSearch import GridSearch
 
 # The actual script
-ratio = 0.1; #Default is 0.1
+ratio = 0.01; #Default is 0.1
 
 
-trainData = TinyCifar10Dataset("../data",'train',ratio)
-valData = TinyCifar10Dataset("../data",'val',ratio)
-testData = TinyCifar10Dataset("../data",'test',ratio)
+trainData = TinyCifar10Dataset("../data",'train')
+valData = TinyCifar10Dataset("../data",'val')
+testData = TinyCifar10Dataset("../data",'test')
 trainSet = ImageVectorizer(trainData)
 valSet = ImageVectorizer(valData)
 testSet = ImageVectorizer(testData)
 
 
-GridSearch().gridSearch(trainSet, valSet, testSet);
+GridSearch(range(1,40,3), ['l1','l2']).gridSearch(trainSet, valSet, testSet)
 print() # only here to set breakpoint
