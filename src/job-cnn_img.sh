a8b3f3d6b3f8d52b04fa7f3d81eb2c733a378bf5
Hightower999999 <gl@mercado.co.at>
2016-12-11 14:29:55 +0100

    added 2nd cat pic

--- src/job-cnn_img.sh
+++ src/job-cnn_img.sh
@@ -8,3 +8,4 @@ cd group1
 cd submissions
 cd assignment2
 python3 cnn_classify_image.py --means 125.31 122.91 113.8 --stds 63.05 62.16 66.74 --model model_cnn_best.h5 --image cat.jpg
+python3 cnn_classify_image.py --means 125.31 122.91 113.8 --stds 63.05 62.16 66.74 --model model_cnn_best.h5 --image cat_new.jpg
