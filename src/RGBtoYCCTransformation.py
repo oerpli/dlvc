296497e9e79f401b3b3106b011ba844d72ca3a79
alg0627958 <gl@mercado.co.at>
2017-01-27 15:37:12 +0100

    Added color space transformation

---
+++ src/RGBtoYCCTransformation.py
@@ -0,0 +1,28 @@
+import numpy as np
+import skimage.transform  as t
+import scipy as sp
+from SampleTransformation import SampleTransformation
+
+class RGBtoYCCTransformation(SampleTransformation):
+    # Converts from RGB image space to YCC color space
+
+    def apply(self, sample):
+        # Apply the transformation and return the transformed version.
+
+        (rows,cols, x) = sample.shape
+        _sample = np.copy(sample)
+        for row in range(0, rows):
+            for col in range(0, cols):
+                 y,cb,cr = RGBtoYCCTransformation._ycc(sample[row,col,0],sample[row,col,1],sample[row,col,2])
+                 _sample[row,col,0] = y
+                 _sample[row,col,1] = cb
+                 _sample[row,col,2] = cr
+    
+        return  _sample
+
+    @staticmethod
+    def _ycc(r, g, b): # in (0,255) range
+        y = .299*r + .587*g + .114*b
+        cb = 128 -.168736*r -.331364*g + .5*b
+        cr = 128 +.5*r - .418688*g - .081312*b
+        return y, cb, cr
