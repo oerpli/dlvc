dcf52f19a35bfe0859ea7e75ee26a306cdb3d52e
alg0627958 <gl@mercado.co.at>
2017-01-27 17:16:59 +0100

    Only resize if necessary

--- src/RandomCropTransformation.py
+++ src/RandomCropTransformation.py
@@ -4,17 +4,14 @@ from SampleTransformation import SampleTransformation
 
 class RandomCropTransformation(SampleTransformation):
     # Randomly crop samples to a given size.
-    W = int
-    H = int
-    varyCrop = bool # varying the crop between the values and the whole image
 
-
-    def __init__(self, width, height, varyCrop):
+    def __init__(self, width, height, varyCrop, prob):
         # Constructor.
         # Images are cropped randomly to the specified width and height.
         self.W = width
         self.H = height
-        self.varyCrop = varyCrop
+        self.varyCrop = varyCrop # varying instead of fixed crop
+        self.prob = prob;       #probabilit of applying crop
         if varyCrop and self.H != self.W:
                 raise NameError("Can't apply vary crop, width and height values differ")
 
@@ -26,13 +23,16 @@ class RandomCropTransformation(SampleTransformation):
         if rows < self.H or cols < self.W:
                 raise NameError("Can't apply crop, image too small")
         else:
-            if (self.varyCrop):
-                sizeMax = min(rows,cols)
-                sizeX = random.randint(self.W, sizeMax);
-                sizeY = sizeX
+            if random.random() < self.prob:
+                if (self.varyCrop):
+                    sizeMax = min(rows,cols)
+                    sizeX = random.randint(self.W, sizeMax);
+                    sizeY = sizeX
+                else:
+                   sizeX = self.H
+                   sizeY = self.W
+                x = random.randint(0, rows - sizeX)
+                y = random.randint(0, cols - sizeY)
+                return sample[x:x + sizeX,y:y + sizeY,:]
             else:
-               sizeX = self.H
-               sizeY = self.W
-            x = random.randint(0, rows - sizeX)
-            y = random.randint(0, cols - sizeY)
-            return sample[x:x + sizeX,y:y + sizeY,:]
+                return sample
