18dee8fd1f0b992a41db65e8f8c3314456e44447
Abraham Hinteregger <oerpli@hotmail.com>
2017-01-29 17:52:40 +0100

    test_data_augmentation

--- src/test_data_augmentation.py
+++ src/test_data_augmentation.py
@@ -9,39 +9,40 @@ from VerticalMirroringTransformation import VerticalMirroringTransformation
 from PIL import Image
 
 import numpy as np
+import os
 
 imageFileName = "cat.jpg"
 
 print("Loading image ...")
 
-crop = RandomCropTransformation(10,10, True)
+crop = RandomCropTransformation(25,25,0.5, True)
 resize = ResizeImageTransformation(32)
 fliph = HorizontalMirroringTransformation(0.5)
 flipv = VerticalMirroringTransformation(0.5)
-affine = RandomAffineTransformation(20,15,15)
+affine = RandomAffineTransformation(15,10,10,0.2)
+
 transformationSequence = TransformationSequence()
+transformationSequence.add_transformation(resize)
 #transformationSequence.add_transformation(flipv)
-#transformationSequence.add_transformation(affine)
-#transformationSequence.add_transformation(fliph)
+transformationSequence.add_transformation(fliph)
+transformationSequence.add_transformation(affine)
 transformationSequence.add_transformation(crop)
 transformationSequence.add_transformation(resize)
 
-for i in range(0,5):
+for i in range(0,20):
     img = Image.open(imageFileName)
     image = np.array(img)
     print("  Input shape: {}".format(image.shape))
 
-
-
-
-
     image = transformationSequence.apply(image)
 
+
+    #from matplotlib import pyplot as plt
+    #plt.imshow(np.copy(image).astype('uint8'), interpolation='nearest')
+    #plt.show()
+
     #transformedImage = Image.frombuffer("RGBX", image.shape[0:2], image)
-
-    #transformedImage.save("new_"+imageFileName, "JPEG")
-
-
-    from matplotlib import pyplot as plt
-    plt.imshow(np.copy(image).astype('uint8'), interpolation='nearest')
-    plt.show()
+    im = Image.fromarray(np.copy(image).astype('uint8'))
+    #im = Image.frombuffer("RGBX", image.shape[0:2], image)
+    path = os.getcwd() + "\\augmented_samples\\" + str(i) + "_" +imageFileName
+    im.save(path, "JPEG")
