1937419949da49c86bc8ca703badc936ad02cb77
Hightower999999 <gl@mercado.co.at>
2017-01-31 22:24:48 +0100

    zum model mit dropout gewchselt

--- src/train_best_model.py
+++ src/train_best_model.py
@@ -2,12 +2,14 @@ from keras.models import Sequential
 import keras.layers as Lay
 import keras as k
 from keras.regularizers import l2
-from keras.layers import Dropout
+from keras.layers import Dropout, InputLayer, Activation
 import operator
 
 
 from keras.utils.np_utils import to_categorical
 from keras.optimizers import SGD
+from keras.layers.normalization import BatchNormalization
+
 from ImageVectorizer import ImageVectorizer
 from MiniBatchGenerator import MiniBatchGenerator
 from TinyCifar10Dataset import TinyCifar10Dataset
@@ -26,9 +28,13 @@ from RandomAffineTransformation import RandomAffineTransformation
 from ResizeImageTransformation import ResizeImageTransformation
 from SubtractionTransformation import SubtractionTransformation
 from RGBtoYCCTransformation import RGBtoYCCTransformation
+from RandomGrayScaleTransformation import RandomGrayScaleTransformation
 
 from VerticalMirroringTransformation import VerticalMirroringTransformation
 from HDF5FeatureVectorDataset import HDF5FeatureVectorDataset
+
+fileNameModel = "m_do_best_model.h5"
+
 
 
 print("Loading Cifar10Dataset ...")
@@ -41,27 +47,25 @@ dataSetName = 'test'
 test = Cifar10Dataset(dir,dataSetName)
 
 
-#RGBtoYCCTransformation.apply(train.data);
-#RGBtoYCCTransformation.apply(val.data);
-#RGBtoYCCTransformation.apply(test.data);
-
-convRGBtoYCC = RGBtoYCCTransformation()
+#flipv = VerticalMirroringTransformation(0.5) # bad for performance
 floatCast = FloatCastTransformation()
+convRGBtoYCC = RGBtoYCCTransformation()
 offset = PerChannelSubtractionImageTransformation.from_dataset_mean(train)
 scale = PerChannelDivisionImageTransformation.from_dataset_stddev(train)
-affine = RandomAffineTransformation(20,10,10,0.7)
-crop = RandomCropTransformation(25,25, 0.6,True)
+crop = RandomCropTransformation(25,25, 0.25, True)
 resize = ResizeImageTransformation(32)
 fliph = HorizontalMirroringTransformation(0.5)
-flipv = VerticalMirroringTransformation(0.5)
+affine = RandomAffineTransformation(15,10,10,0.2)
+gray = RandomGrayScaleTransformation(0.2)
+
 
 trainingTransformationSequence = TransformationSequence()
-#trainingTransformationSequence.add_transformation(flipv)  decreases performance
 trainingTransformationSequence.add_transformation(fliph)
 trainingTransformationSequence.add_transformation(crop)
+trainingTransformationSequence.add_transformation(resize)
 trainingTransformationSequence.add_transformation(affine)
 #trainingTransformationSequence.add_transformation(convRGBtoYCC)
-#trainingTransformationSequence.add_transformation(resize)
+trainingTransformationSequence.add_transformation(gray)
 trainingTransformationSequence.add_transformation(floatCast)
 trainingTransformationSequence.add_transformation(offset)
 trainingTransformationSequence.add_transformation(scale)
@@ -69,8 +73,9 @@ trainingTransformationSequence.add_transformation(scale)
 testingTransformationSequence = TransformationSequence()
 #testingTransformationSequence.add_transformation(convRGBtoYCC)
 testingTransformationSequence.add_transformation(floatCast)
-testingTransformationSequence.add_transformation(offset) 
+testingTransformationSequence.add_transformation(offset)
 testingTransformationSequence.add_transformation(scale)
+
 
 
 print("Setting up preprocessing ...")
@@ -88,48 +93,55 @@ train_batch = MiniBatchGenerator(train,64,trainingTransformationSequence)
 val_batch = MiniBatchGenerator(val,100,testingTransformationSequence)
 test_batch = MiniBatchGenerator(test,100,testingTransformationSequence)
 
+#train_batch.create()
+#val_batch.create()
+#test_batch.create()
 print(" [train] {} samples, {} minibatches of size {}".format(train.size(), train_batch.nbatches(), train_batch.batchsize()))
 print(" [val]   {} samples, {} minibatches of size {}".format(val.size(), val_batch.nbatches(), val_batch.batchsize()))
 print()
 print("Initializing CNN and optimizer ...")
 
-fileNameModel = "best_model.h5"
 
-weightDecay = 0.005
-dropOutProbability = 0.0
-learningRate = 0.001
-
-fileNameModel = "best_model_.h5"
+weightDecay = 0.001
+dropOutProbability = 0.25
+learningRate = 0.02
 allTimeBestAccuracy = 0.0
-dropOutProbability = 0.14;
+
+#for _dropOutProbability in range(10,12, 4): # fixed at 0.14 for the moment
+    #dropOutProbability = _dropOutProbability / 100.0;
 print("Using values WD:{} LR:{} and DropOut:{}".format(weightDecay,learningRate,dropOutProbability))
 
-print("Using Tranformations:")
-for tranformationNo in range(0,trainingTransformationSequence.size()):
-    print("\t{}".format(trainingTransformationSequence.get_transformation(tranformationNo)))
-
 model = Sequential()
-#Trying to add dropout to input - no improvement, sinse it basically adding noise to the image
+#Trying to add dropout to input - no improvement, sinse it basically adding
+#noise to the image
 #model.add(Dropout(dropOutProbability,input_shape = (32,32,3)))
-#model.add(Lay.Convolution2D(16,3,3,W_regularizer = l2(weightDecay),border_mode='same',activation='relu',dim_ordering="tf"))
+#model.add(Lay.Convolution2D(16,3,3,W_regularizer =
+#l2(weightDecay),border_mode='same',activation='relu',dim_ordering="tf"))
 
-model.add(Lay.Convolution2D(16,3,3,input_shape = (32,32,3),W_regularizer = l2(weightDecay),border_mode='same',activation='relu',dim_ordering="tf"))
-model.add(Lay.MaxPooling2D((2,2),strides=(2,2),dim_ordering="tf"))
-model.add(Dropout(dropOutProbability))
-model.add(Lay.Convolution2D(32,3,3,W_regularizer = l2(weightDecay),border_mode='same',activation = 'relu',dim_ordering="tf"))
-model.add(Lay.MaxPooling2D((2,2),strides=(2,2),dim_ordering="tf"))
-model.add(Dropout(dropOutProbability))
-model.add(Lay.Convolution2D(32,3,3,W_regularizer = l2(weightDecay),border_mode='same',activation = 'relu',dim_ordering="tf"))
-model.add(Lay.MaxPooling2D((2,2),strides=(2,2),dim_ordering="tf"))
-model.add(Dropout(dropOutProbability))
+def AddConv(n,r):
+    model.add(Lay.Convolution2D(n,r,r,W_regularizer = l2(weightDecay),border_mode='same',dim_ordering="tf",init='he_normal', activation='relu'))
+def AddPool(n,s):
+    model.add(Lay.MaxPooling2D((n,n),strides=(s,s),dim_ordering="tf"))
+    #model.add(BatchNormalization())
+
+model.add(InputLayer((32,32,3)))
+AddConv(32,3)
+AddPool(2,2)
+AddConv(64,3)
+AddPool(2,2)
+AddConv(128,3)
+AddConv(256,3)
+AddPool(2,2)
 model.add(Lay.Flatten())
+model.add(Lay.Dense(512,activation = 'relu',init='he_normal'))
+model.add(Dropout(dropOutProbability))
 model.add(Lay.Dense(output_dim = 10,W_regularizer = l2(weightDecay),activation = 'softmax'))
 
 
 
 sgd = SGD(lr=learningRate, decay=0, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy',optimizer=sgd, metrics=["accuracy"])
-#model.summary()
+model.summary()
 print()
 
 
@@ -182,51 +194,3 @@ for epoch in range(0,epochs):
         print("Best validation accuracy: {:02.2f} (epoch {})".format(bestAccuracy,bestAccuracyAtEpoch))
         break
 
-
-#Testing need to be removed
-
-print("")
-print("Testing model on test set ...")
-print("  [test] {} samples, {} minibatches of size {}".format(test.size(), test_batch.nbatches(), test_batch.batchsize()))
-
-#model.load_weights("./" + fileNameModel)
-model = k.models.load_model("./" + fileNameModel)
-
-test_batch.shuffle()
-acc_test = []
-m_acc_test = 0.0
-
-pred = [0] * 10
-classcount = [0] * 10
-wpred = [0] * 10
-
-for bid in range(0,test_batch.nbatches()):
-    b = test_batch.batch(bid)
-    # test classifier
-    features = b[0]
-    labels = to_categorical(b[1],10)
-    metrics = model.test_on_batch(features, labels)
-    y = model.predict_on_batch(features)
-    # store validation accuracy
-    acc_test.append(metrics[1])
-    # compute mean of accurracy
-    m_acc_test = np.mean(acc_test)
-
-    correct = b[1]
-    for i in range(0,features.shape[0]):
-        classcount[correct[i]] += 1
-        max_index, max_value = max(enumerate(y[i]), key = operator.itemgetter(1))
-        if correct[i] == max_index:
-            pred[correct[i]] += 1
-        else:
-            wpred[max_index] += 1
-
-acc = []
-for i in range(0,10):
-    acc.append(pred[i]/classcount[i])
-
-print("  Accuracy: {:0.2%}".format(m_acc_test))
-print("  Accuracy per class: {}".format(niceList(acc)))
-print("  Misclassifications end up in class: {}".format(wpred))
-
-print("Done")
